bd7bb2cb8568c1273fcbbdebf990eb4aa5c5cfab
silambarasan <hellosimbu2023@gmail.com>
2024-05-23 00:33:57 +0530

    form

--- model/userModel.js
+++ model/userModel.js
@@ -5,21 +5,21 @@ const userSchema = new mongoose.Schema({
 
     firstName: { type: String},
     email: { type: String},
-    profileImage: { type: String },
-    cv: { type: String },
-    dateOfBirth: { type: Date},
-    gender: { type: String },
-    jobRole: { type: String, enum: ['designer', 'developer'] },
-    startDate: { type: Date },
-    endDate: { type: Date},
-    Preferred_Meeting_Time: {
-        type: String // Storing as string for simplicity
-    },
-    Subscribe_Newsletter: {
-        type: Boolean,
-        default: false
-    },
-    interests: [{ type: String, enum: ['sports', 'music', 'travel', 'cooking', 'reading'] }] 
+    // profileImage: { type: String },
+    // cv: { type: String },
+    // dateOfBirth: { type: Date},
+    // gender: { type: String },
+    // jobRole: { type: String, enum: ['designer', 'developer'] },
+    // startDate: { type: Date },
+    // endDate: { type: Date},
+    // Preferred_Meeting_Time: {
+    //     type: String // Storing as string for simplicity
+    // },
+    // Subscribe_Newsletter: {
+    //     type: Boolean,
+    //     default: false
+    // },
+    // interests: [{ type: String, enum: ['sports', 'music', 'travel', 'cooking', 'reading'] }] 
 
 })
 
